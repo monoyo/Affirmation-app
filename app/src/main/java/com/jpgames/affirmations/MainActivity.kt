557ce6e8b512100a5be4b6054007e31effd86561
Józef Próchniewicz <jozek.prochniewicz@gmail.com>
2022-03-01 07:26:05 +0100

    The affirmation app created with google

---
+++ app/src/main/java/com/jpgames/affirmations/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jpgames.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.jpgames.affirmations.adapter.ItemAdapter
+import com.jpgames.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
